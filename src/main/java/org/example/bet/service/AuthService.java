c635e48d2dc36f5636e88c56706025b96754c9c6
Daniil Kostrykin <daniilkostrykin1@gmail.com>
2025-11-28 13:26:21 +0300

    make prediction

--- src/main/java/org/example/bet/service/AuthService.java
+++ src/main/java/org/example/bet/service/AuthService.java
@@ -4,7 +4,7 @@ import org.example.bet.domain.User;
 import org.example.bet.domain.UserRole;
 import org.example.bet.models.form.RegistrationForm;
 import org.example.bet.repository.UserRepository;
-import org.springframework.security.crypto.password.PasswordEncoder;
+//import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import lombok.RequiredArgsConstructor;
